de5c5e8c10a93f2bdc6e79c47dad4ae659437257
Alex Chenot <alex@chenot.dev>
2025-07-30 23:35:55 -0600

    feat: add skeleton for digraph

--- lisp-network.lisp
+++ lisp-network.lisp
@@ -1,3 +1,10 @@
 ;;;; lisp-network.lisp
 
 (in-package #:lisp-network)
+
+(defclass digraph ()
+    ((nodes :initform (make-hash-table :test 'equal) :reader nodes)
+     (pred :initform (make-hash-table :test 'equal) :reader pred)
+     (succ :initform (make-hash-table :test 'equal) :reader succ)))
+
+(defun make-digraph () (make-instance 'digraph))
