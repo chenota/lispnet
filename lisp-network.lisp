62d9551ac382826a757634c59c05ef6c08f2ead4
Alex Chenot <alex@chenot.dev>
2025-07-31 19:41:15 -0600

    feat: added nodes to digraph object

--- lisp-network.lisp
+++ lisp-network.lisp
@@ -2,9 +2,41 @@
 
 (in-package #:lisp-network)
 
+(defun gethash-or (key table default)
+  (multiple-value-bind
+      (val exists)
+      (gethash key table)
+    (if
+     exists
+     val
+     (setf (gethash key table) default))))
+
 (defclass digraph ()
-    ((nodes :initform (make-hash-table :test 'equal) :reader nodes)
-     (pred :initform (make-hash-table :test 'equal) :reader pred)
-     (succ :initform (make-hash-table :test 'equal) :reader succ)))
+    ((nodes :initform (make-hash-table :test 'equal))
+     (succ :initform (make-hash-table :test 'equal))
+     (pred :initform (make-hash-table :test 'equal))))
 
 (defun make-digraph () (make-instance 'digraph))
+
+(defmethod add-node ((d digraph) node &rest args)
+  (unless (evenp (length args)) (error "expected even number of arguments"))
+  (let
+      ((table (gethash-or node (slot-value d 'nodes) (make-hash-table :test 'equal))))
+    (loop for (k v) on args by #'cddr do
+            (if
+             (keywordp k)
+             (setf (gethash k table) v)
+             (error "property keys must be keywords")))))
+
+(defmethod node-property ((d digraph) node key)
+  (unless (keywordp key) (error "property keys must be keywords"))
+  (multiple-value-bind
+      (node node-exists)
+      (gethash node (slot-value d 'nodes))
+    (if
+     node-exists
+     (multiple-value-bind
+         (value value-exists)
+         (gethash key node)
+       (values value t value-exists))
+     (values nil nil nil))))
