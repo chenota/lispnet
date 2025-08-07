f97c14f38bbfef6e548a6c24a24dec9e58c8e027
Alex Chenot <alex@chenot.dev>
2025-08-06 21:08:27 -0600

    feat: add edge-property-p function

--- src/predicate.lisp
+++ src/predicate.lisp
@@ -16,10 +16,18 @@
     (declare (ignore value))
     (first exists-path)))
 
-(defmethod property-p ((d digraph) node key)
+(defmethod node-property-p ((d digraph) node key)
   (unless (keywordp key) (error "property keys must be keywords"))
   (multiple-value-bind
       (property exists-path)
       (gethash-multi (slot-value d 'nodes) node key)
     (declare (ignore property))
     (values (first exists-path) (second exists-path))))
+
+(defmethod edge-property-p ((d digraph) begin end key)
+  (unless (keywordp key) (error "property keys must be keywords"))
+  (multiple-value-bind
+      (property exists-path)
+      (gethash-multi (slot-value d 'succ) begin end key)
+    (declare (ignore property))
+    (values (first exists-path) (and (second exists-path) (third exists-path)))))
